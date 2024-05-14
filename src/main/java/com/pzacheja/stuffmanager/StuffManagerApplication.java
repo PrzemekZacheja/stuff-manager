98941f6d5b34443f899892ee35f658e0c308f7b8
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-05-14 22:02:20 +0200

    First commit

---
+++ src/main/java/com/pzacheja/stuffmanager/StuffManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pzacheja.stuffmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuffManagerApplication.class, args);
+	}
+
+}
